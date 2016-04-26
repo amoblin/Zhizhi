c9e30f030c1695316a3893dd5af3988c8dc27382
amoblin@mbp <amoblin@gmail.com>
2016-04-26 12:07:14 +0800

    Initial Commit

---
+++ Zhizhi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zhizhi
+//
+//  Created by amoblin on 16/4/25.
+//  Copyright © 2016年 marboo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
